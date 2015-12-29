763c992b1d084145424fae4d6d06ec910db7d78a
nobe4 <haffreinguev@gmail.com>
2015-12-29 17:17:31 +0100

    Update vagrant aliases

--- vagrant/aliases.zsh
+++ vagrant/aliases.zsh
@@ -1,15 +1,31 @@
 # vagrant alias
 alias vre="vagrant halt && tm vagrant up"
 
+# Check if the current folder has a vagrant environment
+_vagrant_check(){
+	vagrantStatus="$(vagrant status 2>&1)"
+	if echo $vagrantStatus | grep -q 'environment' ; then
+		echo 'Not a vagrant environment, exiting ...'
+		return 0
+  else
+    return 1
+	fi
+}
+
+_is_vagrant_up(){
+	vagrantStatus="$(vagrant status 2>&1)"
+	if echo $vagrantStatus | grep -q 'saved\|aborted\|poweroff' ; then
+		return 0
+  else
+    return 1
+	fi
+}
+
 # Custom ssh function
 vsh(){
 	# getting all output of the vagrant status command
-	vagrantStatus="$(vagrant status 2>&1)"
-	if echo $vagrantStatus | grep -q 'environment' ; then
-		echo '\e[34m[VSH] \e[0mNot a vagrant environment, exiting ...'
-		return;
-	fi
-	if echo $vagrantStatus | grep -q 'saved\|aborted\|poweroff' ; then
+	if _vagrant_check; then return; fi
+	if !_is_vagrant_up; then
 		echo '\e[34m[VSH] \e[0mRestoring the machine state ...'
 		vagrant up
 	fi
@@ -17,3 +33,18 @@ vsh(){
 	vagrant ssh
 }
 
+vup(){
+	if _vagrant_check; then return; fi
+	if ! _is_vagrant_up; then
+		vagrant up
+	fi
+  notify "Vagrant started"
+}
+
+vhalt(){
+	if _vagrant_check; then return; fi
+	if _is_vagrant_up; then
+		vagrant halt
+	fi
+  notify "Vagrant halted"
+}
