b3c8124ae3e73228f023d5e8eff5bf9e7fa05f2c
nobe4 <haffreinguev@gmail.com>
2016-02-08 09:56:02 +0100

    Do not use the precmd

--- prompt/precmd.zsh
+++ prompt/precmd.zsh
@@ -1,6 +1,6 @@
 # custom prepromt command to display pwd if nothing inputted
-precmd() {
-  _L="$(history | tail -n 1)";
-  [ "$_L" != "$_X"  ] || echo $PWD && _X="$_L"
-  unset _L
-}
+# precmd() {
+  # _L="$(history | tail -n 1)";
+  # [ "$_L" != "$_X"  ] || echo $PWD && _X="$_L"
+  # unset _L
+# }
