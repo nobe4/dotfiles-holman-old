a01a8663318336d0eb7e7b0c99f17d8585f073a9
nobe4 <haffreinguev@gmail.com>
2016-03-11 15:28:31 +0000

    Use only default machin for ip copying

--- docker/docker.zsh
+++ docker/docker.zsh
@@ -2,4 +2,4 @@ eval "$(docker-machine env default)"
 
 alias dk="docker"
 alias dc="docker-compose"
-alias dkip="docker-machine ip | /usr/bin/tr -d '\n' | tee >(pbcopy) | xargs echo copied$1"
+alias dkip="docker-machine ip default | /usr/bin/tr -d '\n' | tee >(pbcopy) | xargs echo copied$1"
